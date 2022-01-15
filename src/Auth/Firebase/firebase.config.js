7718d591763f44df7f875c8b4bbe6384cd99e2a0
asifhassan8231 <asifhassan8231@gmail.com>
2022-01-15 19:05:13 +0600

    first commit

---
+++ src/Auth/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA2obA4ZqBdddCvB-Ds7y1Y13_9s4y3hsw",
+    authDomain: "ema-john-plus-d4c1e.firebaseapp.com",
+    projectId: "ema-john-plus-d4c1e",
+    storageBucket: "ema-john-plus-d4c1e.appspot.com",
+    messagingSenderId: "456011548010",
+    appId: "1:456011548010:web:c761d15db4c0f4523c1113"
+};
+
+export default firebaseConfig;
